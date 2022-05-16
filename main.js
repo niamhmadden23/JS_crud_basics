d758006d952985a40f75d23b21120071c37c4cf8
Niamh Madden <niamhmadden15@gmail.com>
2022-05-16 14:00:20 +0100

    simple CRUD complete

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 let form = document.getElementById("form");
 let input = document.getElementById("input")
 let msg = document.getElementById('msg')
+let posts = document.getElementById('posts')
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault()
     console.log('button clicked');
     formValidation()
+ 
   
 })
 
@@ -14,6 +16,41 @@ let formValidation = () =>{
         msg.innerHTML = 'Post cannot be blank'
     } else{
         msg.innerHTML = ''
+        acceptData()
+        createPost();
+      
     }
+}
+
+let data = {};
+let acceptData = () =>{
+    data['text'] = input.value;
+    console.log(data)
+    
+}
+
+let createPost = () =>{
+    posts.innerHTML += `
+    
+    <div>
+      <p>${data.text}</p>
+      <span class="options">
+        <i onClick="editPost(this)" class="fas fa-edit"></i>
+        <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
+      </span>
+    </div>
+`;
+input.value = ""
+    
+    
+}
+
+let deletePost = (e) =>{
+        e.parentElement.parentElement.remove();
+}
+
+let editPost = (e) =>{
+input.value = e.parentElement.previousElementSibling.innerHTML;
+e.parentElement.parentElement.remove();
 }
 
